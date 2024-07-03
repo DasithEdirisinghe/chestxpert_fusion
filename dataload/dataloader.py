a0197013958bbf9a282e9721e45865c5111d432a
Dasith Edirisinghe <dasith.edirisinghe@rabot.us>
2024-07-03 14:17:32 +0530

    remove image tranform from dataloader

--- dataload/dataloader.py
+++ dataload/dataloader.py
@@ -20,19 +20,12 @@ def create_data_loaders(csv_file, tokenizer, batch_size, valid_size=0.2, shuffle
     # Split the dataset
     train_df, valid_df = train_test_split(data_frame, test_size=valid_size, random_state=random_seed)
 
-    # Define the transform for images
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
-
     # Initialize the CLIP processor
     clip_processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
     # Create datasets
-    train_dataset = ImageCaptionDataset(data_frame=train_df, tokenizer=tokenizer, transform=transform, clip_processor=clip_processor)
-    valid_dataset = ImageCaptionDataset(data_frame=valid_df, tokenizer=tokenizer, transform=transform, clip_processor=clip_processor)
+    train_dataset = ImageCaptionDataset(data_frame=train_df, tokenizer=tokenizer, clip_processor=clip_processor)
+    valid_dataset = ImageCaptionDataset(data_frame=valid_df, tokenizer=tokenizer, clip_processor=clip_processor)
 
     # Create data loaders
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
